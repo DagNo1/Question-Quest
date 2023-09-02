1344e2729c946793741e7f79c238dd178c0b70a2
Dagim <dagimcodes@gmail.com>
2023-09-02 11:23:14 +0300

    Revert last 2 commits on main

--- src/pages/AuthPages/SignIn.jsx
+++ src/pages/AuthPages/SignIn.jsx
@@ -1,18 +1,6 @@
 import AuthPage from "./AuthPage";
 
 const SignIn = () => {
-  const signInWithEmail = (email) => {};
-  const signInWithGoogle = () => {};
-  const authData = {
-    signin: true,
-    header: "Glad to see you back",
-    header2: "let’s start where we left off",
-    text: "Don’t have an account? ",
-    link: "",
-    sign_email: signInWithEmail,
-    sign_google: signInWithGoogle,
-  };
+  const authData = {};
   return <AuthPage data={authData} />;
 };
-
-export default SignIn;
--- src/pages/AuthPages/SignUp.jsx
+++ src/pages/AuthPages/SignUp.jsx
@@ -1,18 +1,6 @@
 import AuthPage from "./AuthPage";
 
 const SignUp = () => {
-  const signUpWithEmail = (email) => {};
-  const signUpWithGoogle = () => {};
-  const authData = {
-    signin: false,
-    header: "Create an account",
-    header2: "Let's get you all set up",
-    text: "Already have an account? ",
-    link: "",
-    sign_email: signUpWithEmail,
-    sign_google: signUpWithGoogle,
-  };
+  const authData = {};
   return <AuthPage data={authData} />;
 };
-
-export default SignUp;
