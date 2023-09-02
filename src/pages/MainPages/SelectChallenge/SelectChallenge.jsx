40e859c021a6d896e39862853eb41cae7c53be43
Dagim <dagimcodes@gmail.com>
2023-09-02 20:41:50 +0300

    Select Pages made responsive

--- src/pages/MainPages/SelectChallenge/SelectChallenge.jsx
+++ src/pages/MainPages/SelectChallenge/SelectChallenge.jsx
@@ -14,23 +14,23 @@ const SelectChallenge = () => {
     }
   ]
   return (
-    <div className="flex justify-center items-center flex-col my-10 gap-5">
-      <p className="text-white">Select Your Challenge Type</p>
-
+    <div className="flex justify-center items-center flex-col my-10 gap-5 md:my-20 md:gap-10">
+      <p className="text-white md:text-2xl lg:text-3xl">Select Your Challenge Type</p>
+      <div className="flex flex-col gap-5 lg:w-[40rem]">
       {
         choices.map((choice) => (
           <button
             className="text-white/80 border border-white/50  lg:text-xs motion-safe:hover:scale-105 
-            focus:bg-purple/50 focus:border-white  font-medium rounded-md
-            text-sm px-12 md:px-5 md:text-xs py-2 text-center mb-2 max-md:px-4 max-md:py-[0.8rem]
-            w-80 bg-gray-500/10
+            focus:bg-purple/50 focus:border-purple  font-medium rounded-md
+            text-sm py-3 mx-10 mb-2 px-4 max-md:py-[0.8rem] text-start 
+            w-72 bg-gray-500/10  md:w-[25rem] md:text-lg lg:w-96 lg:text-xl lg:self-start lg:w-[32rem]
             "
           >
             {choice.text}
           </button>
         ))
       }
-
+      </div>
     </div>
   );
 };
--- src/pages/MainPages/SelectChallenge2/SelectChallenge2.jsx
+++ src/pages/MainPages/SelectChallenge2/SelectChallenge2.jsx
@@ -10,26 +10,26 @@ const SelectChallenge2 = () => {
       text: "20-30 Challenges"
     },
     {
-      text: "30-4s0 Challenges"
+      text: "30-40 Challenges"
     }
   ]
   return (
-    <div className="flex justify-center items-center flex-col my-10 gap-10">
-      <p className="text-white">Select Your Challenge Type</p>
+    <div className="flex justify-center items-center flex-col my-10 gap-10 md:gap-5">
+      <p className="text-white md:text-2xl lg:text-3xl">Select Your Challenge Type</p>
       <button
-        className="text-white/80 border border-white/50  lg:text-xs motion-safe:hover:scale-105 
-          bg-purple/50 border-white  font-medium rounded-md text-sm px-12 md:px-5 md:text-xs 
-          py-2 text-center mb-2 max-md:px-4 max-md:py-[0.8rem] w-80
-          "
+        className="text-white/80 border border-white/50 lg:text-xs
+        bg-purple/50  font-medium rounded-md
+        text-sm py-3 text-center mx-10 mb-2 max-md:px-4 max-md:py-[0.8rem]
+        w-72  md:w-[25rem] md:text-lg lg:w-[30rem] lg:text-xl"
       >Multiple Question</button>
-      <div className="grid grid-cols-2 gap-2">
+      <div className="grid grid-cols-2 gap-2 md:gap-5">
         {
           choices.map((choice) => (
             <button
               className="text-white/80 border border-white/50  lg:text-xs motion-safe:hover:scale-105 
-          focus:bg-purple/50 focus:border-white  font-medium rounded-md
-          text-sm px-12 md:px-5 md:text-xs py-2 text-center mb-2 max-md:px-4 max-md:py-[0.8rem]
-          w-40 bg-gray-500/10
+          focus:bg-purple/50 focus:border-white font-medium rounded-md
+          text-sm px-12 md:px-5 py-2 text-center mb-2 max-md:px-4 max-md:py-[0.8rem]
+          w-40 bg-gray-500/10 md:w-48 md:text-md lg:text-lg lg:w-56
           "
             >
               {choice.text}
