bd8c1cc6b168b056c5e80e1bde926706d9fd3c9b
Dagim <dagimcodes@gmail.com>
2023-09-02 12:23:53 +0300

    Minor fixes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import NavigationBar from "../../components/NavigationBar";
 import Banner from "./Banner";
 import Preview from "./Preview";
 
@@ -6,6 +7,7 @@ const Home = () => {
     <div className="bg-netural min-w-screen min-h-screen">
       <div className="bgGradient w-full min-h-screen">
         <div className="radial-gradient" />
+        <NavigationBar/>
         <Banner />
         <Preview />
       </div>
