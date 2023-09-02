54aa6af3ccb2f246ace101553cf3eda85ee45a76
Dagim <dagimcodes@gmail.com>
2023-09-02 13:31:12 +0300

    Main Layout

--- src/pages/MainPages/MainLayout.jsx
+++ src/pages/MainPages/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import NavigationBar from "../../components/NavigationBar";
 
 const MainLayout = () => {
   return (
-    <div>
-      <Outlet />
+    <div className="bg-netural w-full max-lg:min-h-screen">
+      <div className="bgGradient w-full max-lg:min-h-screen">
+        <NavigationBar seeThrough={false}/>
+        <Outlet className="w-full"/>
+      </div>
     </div>
   );
 };
