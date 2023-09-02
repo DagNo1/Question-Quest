a3c21edf57ae3bdaf97dc4a4277b1256ffbfc4f6
Dagim <dagimcodes@gmail.com>
2023-09-02 11:10:19 +0300

    Sign in/up

--- src/pages/AuthPages/SignIn.jsx
+++ src/pages/AuthPages/SignIn.jsx
@@ -1,7 +1,17 @@
 import AuthPage from "./AuthPage";
 
 const SignIn = () => {
-  const authData = {};
+  const signInWithEmail = (email) => {};
+  const signInWithGoogle = () => {};
+  const authData = {
+    signin: true,
+    header: "Glad to see you back",
+    header2: "let’s start where we left off",
+    text: "Don’t have an account? ",
+    link: "",
+    sign_email: signInWithEmail,
+    sign_google: signInWithGoogle,
+  };
   return <AuthPage data={authData} />;
 };
 
